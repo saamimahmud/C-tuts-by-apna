2fd9aece94dad8125e05adbe28f581b96de00077
saamimahmud <saamimahmud123@gmail.com>
2024-05-10 23:58:05 +0530

    first commit

---
+++ perfect_square.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    printf("\n\n\t\tStudytonight - Best place to learn\n\n\n");
+
+    // variable declaration
+    int i, number;
+
+    // take user input
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    // loop to check number is perfect square or not
+    for(i = 0; i <= number; i++)
+    {
+        if(number == i*i)
+        {
+            printf("\n\n\n\t\t\t%d is a perfect square\n\n\n", number);
+
+            printf("\ntCoding is Fun !\n\n\n");
+            return 0;   // same as using break in this case to end the program
+        }
+    }
+    printf("\n%d is not a perfect square\n\n", number);
+    return 0;
+}
